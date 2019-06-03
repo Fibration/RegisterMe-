6aee9ae7a8f4f3aefe695d55449a3cff9661ec86
Fibration <joseph6.626@gmail.com>
2019-06-03 22:52:04 +1000

    content for comments

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Barack Obama'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username':'Jesus'},
+            'body': 'wafer'
+        },
+        {
+            'author': {'username':'Judas'},
+            'body': 'hot'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
