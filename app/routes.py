b51c072c5c41b2fe1de7aea0626eb8b1e6ee1900
pkios <phil.kiossoglou@gmail.com>
2019-06-03 22:50:28 +1000

    Second page of tutorial, p2

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,21 @@
-
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': "Bob Jones"}
+    posts = [
+        {
+            'author': {'username': 'James'},
+            'body': "Beautiful day in Portland"
+        },
+        {
+            'author': {'username': 'Harold'},
+            'body': "I have not watched the Avengers movie because I have been living under a rock"
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 @app.route('/philtest')
 def philtest():
