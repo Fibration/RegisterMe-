72be9cd6a7f9c9a451917a298e630a613fc2792d
Fibration <joseph6.626@gmail.com>
2019-06-26 22:11:38 +1000

    first database migration; of chapter 4

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # load dotenv
 APP_ROOT = os.path.join(os.path.dirname('config.py'), '..')
@@ -8,3 +9,6 @@ load_dotenv(dotenv_path)
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'whatever'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
